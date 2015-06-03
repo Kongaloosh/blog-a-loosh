948fd098d13230bd26277a018c5fc1caaf76a755
Alex <konga.kearney@gmail.com>
2015-06-02 18:26:00 -0600

    pointing to .html template

--- kongaloosh.py
+++ kongaloosh.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "Hello, world"
+    return render_template('/index.html')
 
 
 if __name__ == "__main__":
