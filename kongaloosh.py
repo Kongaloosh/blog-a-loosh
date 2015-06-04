f1b88b3e0b811d06ae2783774e3ed34a28b32fc5
Alex <konga.kearney@gmail.com>
2015-06-04 08:47:27 -0600

    sliced up bear's project

--- kongaloosh.py
+++ kongaloosh.py
@@ -26,6 +26,65 @@ def init_db():
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
+##################WEB MENTION######################
+
+
+def processWebmention(sourceURL, targetURL, vouchDomain=None):
+    result = False
+    r      = requests.get(sourceURL, verify=False)
+    if r.status_code == requests.codes.ok:
+        mentionData = { 'sourceURL':   sourceURL,
+                        'targetURL':   targetURL,
+                        'vouchDomain': vouchDomain,
+                        'vouched':     False,
+                        'received':    datetime.date.today().strftime('%d %b %Y %H:%M'),
+                        'postDate':    datetime.date.today().strftime('%Y-%m-%dT%H:%M:%S')
+                      }
+        if 'charset' in r.headers.get('content-type', ''):
+            mentionData['content'] = r.text
+        else:
+            mentionData['content'] = r.content
+
+        if vouchDomain is not None and cfg['require_vouch']:
+            mentionData['vouched'] = processVouch(sourceURL, targetURL, vouchDomain)
+            result                 = mentionData['vouched']
+            app.logger.info('result of vouch? %s' % result)
+        else:
+            result = not cfg['require_vouch']
+            app.logger.info('no vouch domain, result %s' % result)
+
+        mf2Data = Parser(doc=mentionData['content']).to_dict()
+        hcard   = extractHCard(mf2Data)
+
+        mentionData['hcardName'] = hcard['name']
+        mentionData['hcardURL']  = hcard['url']
+        mentionData['mf2data']   = mf2Data
+
+        # Do something with the inbound mention
+
+    return result
+
+
+def mention(sourceURL, targetURL, vouchDomain=None):
+    """Process the Webmention of the targetURL from the sourceURL.
+
+    To verify that the sourceURL has indeed referenced our targetURL
+    we run findMentions() at it and scan the resulting href list.
+    """
+    app.logger.info('discovering Webmention endpoint for %s' % sourceURL)
+
+    mentions = ronkyuu.findMentions(sourceURL)
+    result   = False
+    app.logger.info('mentions %s' % mentions)
+    for href in mentions['refs']:
+        if href != sourceURL and href == targetURL:
+            app.logger.info('post at %s was referenced by %s' % (targetURL, sourceURL))
+
+            result = processWebmention(sourceURL, targetURL, vouchDomain)
+    app.logger.info('mention() returning %s' % result)
+    return result
+
+
 ##################REQUEST ARGS#####################
 
 @app.before_request
@@ -75,6 +134,32 @@ def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
     return redirect(url_for('show_entries'))
+
+@app.route('/webmention', methods=['POST'])
+def handleWebmention():
+    app.logger.info('handleWebmention [%s]' % request.method)
+    if request.method == 'POST':
+        valid  = False
+        source = request.form.get('source')
+        target = request.form.get('target')
+        vouch  = request.form.get('vouch')
+        app.logger.info('source: %s target: %s vouch %s' % (source, target, vouch))
+
+        valid = validURL(target)
+
+        app.logger.info('valid? %s' % valid)
+
+        if valid == requests.codes.ok:
+            if mention(source, target, vouch):
+                return redirect(target)
+            else:
+                if vouch is None and cfg['require_vouch']:
+                    return 'Vouch required for webmention', 449
+                else:
+                    return 'Webmention is invalid', 400
+        else:
+            return 'invalid post', 404
+
 
 if __name__ == "__main__":
     app.run(debug=True)
