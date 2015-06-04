d0b7fc3dd7f1ac31baed25df57fc4f3c5850c1aa
Alex <konga.kearney@gmail.com>
2015-06-03 20:02:24 -0600

    simple micro blog following flaskr

--- kongaloosh.py
+++ kongaloosh.py
@@ -1,12 +1,80 @@
-from flask import Flask, render_template
+# all the imports
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash
+from contextlib import closing
 
+# configuration
+DATABASE = '/tmp/kongaloosh.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'Anubis'
+PASSWORD = 'Munc4kin))'
+
+# create our little application :)
 app = Flask(__name__)
+app.config.from_object(__name__)
 
+##################DATABASE#########################
 
-@app.route("/")
-def index():
-    return render_template('/index.html')
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+##################REQUEST ARGS#####################
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
+################## ROUTING ########################
+
+@app.route('/')
+def show_entries():
+    cur = g.db.execute('select title, text from entries order by id desc')
+    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    return render_template('show_entries.html', entries=entries)
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    g.db.execute('insert into entries (title, text) values (?, ?)',
+                 [request.form['title'], request.form['text']])
+    g.db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
 
 if __name__ == "__main__":
     app.run(debug=True)
