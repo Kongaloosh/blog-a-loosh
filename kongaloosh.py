4e0c7ab7312ae849f8fced854920df17ff17658c
Alex <konga.kearney@gmail.com>
2015-06-06 11:29:14 -0600

    added dates to microformats

--- kongaloosh.py
+++ kongaloosh.py
@@ -191,16 +191,16 @@ def teardown_request(exception):
 
 @app.route('/')
 def show_entries():
-    cur = g.db.execute('select title, text from entries order by id desc')
-    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    cur = g.db.execute('select title, text, date_published from entries order by id desc')
+    entries = [dict(title=row[0], text=row[1],date_published=row[2]) for row in cur.fetchall()]
     return render_template('show_entries.html', entries=entries)
 
 @app.route('/add', methods=['POST'])
 def add_entry():
     if not session.get('logged_in'):
         abort(401)
-    g.db.execute('insert into entries (title, text) values (?, ?)',
-                 [request.form['title'], request.form['text']])
+    g.db.execute('insert into entries (title, text, date_published) values (?, ?, ?)',
+                 [request.form['title'], request.form['text'], datetime.datetime.now()])
     g.db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
--- schema.sql
+++ schema.sql
@@ -2,5 +2,6 @@ drop table if exists entries;
 create table entries (
   id integer primary key autoincrement,
   title text not null,
-  text text not null
+  text text not null,
+  date_published text not null
 );
