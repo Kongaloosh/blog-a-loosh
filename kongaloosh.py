ade997f8e4faa67456c64ae3194190785344e7cb
Alex <konga.kearney@gmail.com>
2015-06-05 21:43:31 -0600

    working on microformats

--- kongaloosh.py
+++ kongaloosh.py
@@ -3,6 +3,15 @@ import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
 from contextlib import closing
+import os
+import redis
+import requests
+import ronkyuu
+import ninka
+import datetime
+
+from mf2py.parser import Parser
+from urlparse import urlparse, ParseResult
 
 # configuration
 DATABASE = '/tmp/kongaloosh.db'
@@ -14,7 +23,7 @@ PASSWORD = 'Munc4kin))'
 # create our little application :)
 app = Flask(__name__)
 app.config.from_object(__name__)
-
+cfg = None
 ##################DATABASE#########################
 
 def init_db():
@@ -84,6 +93,87 @@ def mention(sourceURL, targetURL, vouchDomain=None):
     app.logger.info('mention() returning %s' % result)
     return result
 
+
+def extractHCard(mf2Data):
+    result = { 'name': '',
+               'url':  '',
+             }
+    if 'items' in mf2Data:
+        for item in mf2Data['items']:
+            if 'type' in item and 'h-card' in item['type']:
+                result['name'] = item['properties']['name']
+                if 'url' in item['properties']:
+                    result['url'] = item['properties']['url']
+    return result
+
+def processVouch(sourceURL, targetURL, vouchDomain):
+    """Determine if a vouch domain is valid.
+
+    This implements a very simple method for determining if a vouch should
+    be considered valid:
+    1. does the vouch domain have it's own webmention endpoint
+    2. does the vouch domain have an indieauth endpoint
+    3. does the domain exist in the list of domains i've linked to
+
+    yep, super simple but enough for me to test implement vouches
+    """
+    vouchFile = os.path.join(cfg['basepath'], 'vouch_domains.txt')
+    with open(vouchFile, 'r') as h:
+        vouchDomains = []
+        for domain in h.readlines():
+            vouchDomains.append(domain.strip().lower())
+
+    if vouchDomain.lower() in vouchDomains:
+        result = True
+    else:
+        wmStatus, wmUrl = ronkyuu.discoverEndpoint(vouchDomain, test_urls=False)
+        if wmUrl is not None and wmStatus == 200:
+            authEndpoints = ninka.indieauth.discoverAuthEndpoints(vouchDomain)
+
+            if 'authorization_endpoint' in authEndpoints:
+                authURL = None
+                for url in authEndpoints['authorization_endpoint']:
+                    authURL = url
+                    break
+                if authURL is not None:
+                    result = True
+                    with open(vouchFile, 'a+') as h:
+                        h.write('\n%s' % vouchDomain)
+
+def processWebmention(sourceURL, targetURL, vouchDomain=None):
+    result = False
+    r      = requests.get(sourceURL, verify=False)
+    if r.status_code == requests.codes.ok:
+        mentionData = { 'sourceURL':   sourceURL,
+                        'targetURL':   targetURL,
+                        'vouchDomain': vouchDomain,
+                        'vouched':     False,
+                        'received':    datetime.date.today().strftime('%d %b %Y %H:%M'),
+                        'postDate':    datetime.date.today().strftime('%Y-%m-%dT%H:%M:%S')
+                      }
+        if 'charset' in r.headers.get('content-type', ''):
+            mentionData['content'] = r.text
+        else:
+            mentionData['content'] = r.content
+
+        if vouchDomain is not None and cfg['require_vouch']:
+            mentionData['vouched'] = processVouch(sourceURL, targetURL, vouchDomain)
+            result                 = mentionData['vouched']
+            app.logger.info('result of vouch? %s' % result)
+        else:
+            result = not cfg['require_vouch']
+            app.logger.info('no vouch domain, result %s' % result)
+
+        mf2Data = Parser(doc=mentionData['content']).to_dict()
+        hcard   = extractHCard(mf2Data)
+
+        mentionData['hcardName'] = hcard['name']
+        mentionData['hcardURL']  = hcard['url']
+        mentionData['mf2data']   = mf2Data
+
+        # Do something with the inbound mention
+
+    return result
 
 ##################REQUEST ARGS#####################
 
