839e2187ef26b8df05c52c95a61a44a72365503f
Alex <konga.kearney@gmail.com>
2015-06-06 12:20:51 -0600

    added table for mention testing

--- schema.sql
+++ schema.sql
@@ -1,7 +1,17 @@
 drop table if exists entries;
+drop table if exists mentions;
+
 create table entries (
   id integer primary key autoincrement,
   title text not null,
   text text not null,
   date_published text not null
+);
+
+create table mentions(
+  id integer primary key autoincrement,
+  content text not null,
+  source_url text not null,
+  target_url text not null,
+  post_date text not null
 );
