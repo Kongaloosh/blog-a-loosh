e47bdfd2d0d723e7e73e1b05d2f24f6f1292c7d7
Alex <konga.kearney@gmail.com>
2015-06-17 11:33:36 -0600

    debug

--- kongaloosh.py
+++ kongaloosh.py
@@ -674,8 +674,8 @@ def show_entries():
                 entries.append(e)
         if len(entries) >= 10: break
     entries = sorted(entries, key=itemgetter('dt-published'), reverse=True)
-    e = {'title':'boogaloo'}
-    return render_template('show_entries.html', entries=e)
+    pickle.dump(entries, open('entries', 'wb'))
+    return render_template('show_entries.html', entries=entries)
 
 
 @app.route('/<year>/<month>/<day>/<type>/<name>')
