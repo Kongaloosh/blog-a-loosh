ad24cb1de9eeb8f50daae8db3271c6fdb0c55e72
Alex <konga.kearney@gmail.com>
2015-08-01 23:19:27 -0600

    fixing the broken checker

--- pysrc/webmention/webemention_checking.py
+++ pysrc/webmention/webemention_checking.py
@@ -10,11 +10,16 @@ def get_mentions(url):
     """We fetch all the webe"""
     mentions = []
     try:
-        header = {'target': url}
-        r = requests.get('http://webmention.io/api/mentions', header=header)
+        print(url)
+        r = requests.get('http://webmention.io/api/mentions?target='+url)
         p = r.json()
         for link in p['links']:
             mentions.append(link['data'])
+        print(r)
+        print(p)
     except:
         pass
     return mentions
+
+if __name__ == '__main__':
+    get_mentions('http://kongaloosh.com/e/2015/8/2/this-is-a-test-note-to-use-as-a-source-for-sending-web-mentions')
