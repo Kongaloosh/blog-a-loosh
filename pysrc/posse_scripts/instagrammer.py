312dc6a7d38930e2ed69fb3eed3e80b9bd510cce
Stefan <smartynk@ualberta.ca>
2016-10-09 16:24:47 -0600

    Consolidated config to a .ini file, read by ConfigParser

--- pysrc/posse_scripts/instagrammer.py
+++ pysrc/posse_scripts/instagrammer.py
@@ -1,8 +1,14 @@
 import python
 from instagram.client import InstagramAPI
+import ConfigParser
 
-access_token = "YOUR_ACCESS_TOKEN"
-client_secret = "YOUR_CLIENT_SECRET"
+config = ConfigParser.ConfigParser()
+config.read('config.ini')
+
+
+
+access_token = config.get('Instagram', 'AccessToken')
+client_secret = config.get('Instagram', 'ClientSecret')
 api = InstagramAPI(access_token=access_token, client_secret=client_secret)
 recent_media, next_ = api.user_recent_media(user_id="userid", count=10)
 
