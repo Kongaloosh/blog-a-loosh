501fa6bb117a3a58dac287b0597c0b178a7c27b2
Alex Kearney <hi@kongaloosh.com>
2018-08-29 23:15:20 -0600

    checking for an OS error for reasons I can no longer remember

--- pysrc/file_management/markdown_album_pre_process.py
+++ pysrc/file_management/markdown_album_pre_process.py
@@ -43,9 +43,12 @@ def move(loc, date):
     h_percent = (max_height / float(img.size[1]))               # calculate what percentage the new height is of the old
     w_size = int((float(img.size[0]) * float(h_percent)))       # calculate the new size of the width
     img = img.resize((w_size, max_height), Image.ANTIALIAS)     # translate the image
-    if not os.path.exists(old_prefix+date_suffix):              # if the blog's directory doesn't exist
-        os.makedirs(os.path.dirname(old_prefix+date_suffix))    # make it
-    img.save(old_prefix+date_suffix+file_name.lower())          # image save old_prefix
+    try:
+        if not os.path.exists(old_prefix+date_suffix):              # if the blog's directory doesn't exist
+            os.makedirs(os.path.dirname(old_prefix+date_suffix))    # make it
+        img.save(old_prefix+date_suffix+file_name.lower())          # image save old_prefix
+    except OSError:
+        pass
     # Result:
     # Scaled optimised thumbnail in the blog-source next to the post's json and md files
     # Original-size photos in the self-hosting image server directory
@@ -179,4 +182,46 @@ Robot shop
 [](/images/temp/IMG_6808.JPG)
 @@@
 """
+
+    lines = """
+        I flew down to Wyoming on the weekend to go see the Solar Eclipse. This was a spur of the moment trip; a family friend is an amateur astronomer and was planning on driving down on the weekend to be able to catch the eclipse. One problem with their plan: motel rooms within driving distance of the totality were going for $1200 or more on the evening before the eclipse. One solution: motel rooms within flying distance of the totality were going for 90$ a night the evening before the eclipse. 
+
+        @@@[](/data/2017/8/25/img_6838.jpg)-[](/data/2017/8/25/img_6834.jpg)-[](/data/2017/8/25/img_6837.jpg)@@@
+
+        Instead of driving down, we all flew down from Calgary, stayed the night in Montana, and on the morning of the eclipse flew in to [Riverton, Wyoming](https://en.wikipedia.org/wiki/Riverton,_Wyoming), a town of roughly 11,000 people. 
+
+Why the fuss of trying to get onto the totality line? In many places you could see the sun turn into a sliver during the eclipse, but only if you had the protective eyewear. In a few places stretching across the east to west of the U.S. you were able to see the eclipse in it's totality; for just over two minutes the sun would be entirely blocked by the moon, allowing you to see the sun's corona.
+
+### A Surreal Setting
+
+They really pulled out all the stops in Riverton for the eclipse. There were a couple of people with walkie-talkies and an ATV set up as temporary ground-control. On arrival they pointed you towards a breakfast barbecue they'd setup for all the planes which flew in for the day. 
+
+Apparently we weren't the only people to come up with this plan.
+
+
+
+@@@[](/data/2017/8/25/img_7309.jpg)-[](/data/2017/8/25/img_7302.jpg)@@@
+
+
+
+One of the most interesting aspects was the sheer number of planes. There were a score of private jets on the ramp, including a challenger, and probably close to a hundred small personal planes on the tarmac and grass surrounding the airstrip. A few people were camping next to their planes overnight. A number of the private jets erected shelters and had party games under their wings. 
+
+It was like a tailgate party for plane people. 
+
+11,000 people live in this town and it suddenly had hundreds of millions of dollars in planes sitting on the tarmac, only to leave when the eclipse was over.
+
+### The Most Impressive Sight I've Seen
+
+To view the eclipse we brought a number of welders glasses which we could layer depending on the cloud-cover and intensity of the sunlight. At first you could only see a little dimple in the top of the sun. As the dimple grew and the moon covered more of the sun, it got dimmer and dimmer, but not fast enough that you noticed it immediately. Even when mostly covered, the sun never really seemed like it was covered. 
+
+
+
+@@@
+[](/data/2017/8/25/img_6859.jpg)
+@@@
+
+
+
+    
+    """
     print run(lines, date=None)
