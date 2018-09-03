2a1c1c8dcd730b26fce454a4e338ac32f0133955
Alex Kearney <hi@kongaloosh.com>
2018-09-03 13:57:22 -0600

    the way images were handled was buggy due to the new config setup; patched and commented.

--- pysrc/file_management/markdown_album_pre_process.py
+++ pysrc/file_management/markdown_album_pre_process.py
@@ -12,7 +12,6 @@ config = ConfigParser.ConfigParser()
 config.read('config.ini')
 
 ORIGINAL_PHOTOS_DIR = config.get('PhotoLocations', 'BulkUploadLocation')
-PHOTOS_URL = config.get('PhotoLocations', 'URLPhotos')
 
 old_prefix = config.get('PhotoLocations', 'BlogStorage')
 new_prefix = config.get('PhotoLocations', 'PermStorage')
@@ -36,14 +35,21 @@ def move(loc, date):
         date.month,
         date.day
     )
+    # loc is where the temp image is that we're trying to resize and move
+    # we get the filename--without the relative folder structure---to use in the new savenames
+    file_name = loc[len('/images/temp/'):]                        # remove the '/images/temp/'
+    # we remove '/images/' because it's in both the prefix from the config file and the filename from the wysiwyg edit
+    # todo: check to see if you can re-name the prefix in the config file to make this easier
+    # this creates the absolute filepath
+    target_file_path = new_prefix + loc[len('/images/'):]         # remove the '/images/'
 
-    file_name = loc[13:]                                                    # remove the '/images/temp/'
-    target_file_path = new_prefix + loc[1:]
-    if not os.path.exists(new_prefix+'images/' + date_suffix):              # if the target directory doesn't exist ...
-        os.makedirs(os.path.dirname(new_prefix+'images/'+date_suffix))      # ... make it.
+    # 1. SAVE THE ORIGINAL IMAGE AT ORIGINAL QUALITY
+    if not os.path.exists(new_prefix + date_suffix):              # if the target directory doesn't exist ...
+        os.makedirs(os.path.dirname(new_prefix + date_suffix))      # ... make it.
     img = Image.open(target_file_path)                                      # open the image from the temp
-    img.save(new_prefix+'images/'+date_suffix+file_name.lower())            # open the new location
+    img.save(new_prefix+date_suffix+file_name.lower())            # open the new location
 
+    # 2. RESIZE IMAGE AND SAVE FOR BLOG SERVING
     max_height = 500                                            # maximum height
     img = Image.open(target_file_path)                          # open the image in PIL
     h_percent = (max_height / float(img.size[1]))               # calculate what percentage the new height is of the old
@@ -107,6 +113,7 @@ def run(lines, date=None):
                             ).group()                                   # get the text
 
                             if image_ref.startswith("/images/temp/"):   # if the location is in our temp folder...
+                                print image_ref, date
                                 image_ref = move(image_ref, date)       # ... move and resize photos
                             album += "[%s](%s)" % (alt, image_ref)      # album
                             if index != len(images) - 1:                # if this isn't the last image in the set...
@@ -131,129 +138,7 @@ def run(lines, date=None):
 
 if __name__ == "__main__":
     lines = u'@@@\r\n[](/data/2017/7/4/img_6817.jpg)-\r\n[](/data/2017/7/4/img_0281.jpg)-\r\n[](/data/2017/7/4/img_6786.jpg)\r\n@@@'
-
-    lines_3 = """
-        I flew down to Wyoming on the weekend to go see the Solar Eclipse. This was a spur of the moment trip; a family friend is an amateur astronomer and was planning on driving down on the weekend to be able to catch the eclipse. One problem with their plan: motel rooms within driving distance of the totality were going for $1200 or more on the evening before the eclipse. One solution: motel rooms within flying distance of the totality were going for 90$ a night the evening before the eclipse. 
-
-        @@@[](/data/2017/8/25/img_6838.jpg)-[](/data/2017/8/25/img_6834.jpg)-[](/data/2017/8/25/img_6837.jpg)@@@
-
-        Instead of driving down, we all flew down from Calgary, stayed the night in Montana, and on the morning of the eclipse flew in to [Riverton, Wyoming](https://en.wikipedia.org/wiki/Riverton,_Wyoming), a town of roughly 11,000 people. 
-
-Why the fuss of trying to get onto the totality line? In many places you could see the sun turn into a sliver during the eclipse, but only if you had the protective eyewear. In a few places stretching across the east to west of the U.S. you were able to see the eclipse in it's totality; for just over two minutes the sun would be entirely blocked by the moon, allowing you to see the sun's corona.
-
-### A Surreal Setting
-
-They really pulled out all the stops in Riverton for the eclipse. There were a couple of people with walkie-talkies and an ATV set up as temporary ground-control. On arrival they pointed you towards a breakfast barbecue they'd setup for all the planes which flew in for the day. 
-
-Apparently we weren't the only people to come up with this plan.
-
-
-
-@@@[](/data/2017/8/25/img_7309.jpg)-[](/data/2017/8/25/img_7302.jpg)@@@
-
-
-
-One of the most interesting aspects was the sheer number of planes. There were a score of private jets on the ramp, including a challenger, and probably close to a hundred small personal planes on the tarmac and grass surrounding the airstrip. A few people were camping next to their planes overnight. A number of the private jets erected shelters and had party games under their wings. 
-
-It was like a tailgate party for plane people. 
-
-11,000 people live in this town and it suddenly had hundreds of millions of dollars in planes sitting on the tarmac, only to leave when the eclipse was over.
-
-### The Most Impressive Sight I've Seen
-
-To view the eclipse we brought a number of welders glasses which we could layer depending on the cloud-cover and intensity of the sunlight. At first you could only see a little dimple in the top of the sun. As the dimple grew and the moon covered more of the sun, it got dimmer and dimmer, but not fast enough that you noticed it immediately. Even when mostly covered, the sun never really seemed like it was covered. 
-
-
-
-@@@
-[](/data/2017/8/25/img_6859.jpg)
-@@@
-    """
-
-    lines_2 = """
-This year I went to DLSS and RLSS in Toronto. The introductory talks were probably the best intro to neural nets talks I'd seen: the talks were tight and intuitive without having to water down the technical details. 
-
-
-@@@
-[](/data/2018/8/1/8b4d24be-139a-4a28-ace3-7f9b78123729.jpeg)-
-[](/data/2018/8/1/71f72258-1977-4253-8578-dd338f9f9f7f.jpeg)
-@@@
-
-
-The number of people cramming in for the summer school was surprising. It's really great to see how interest in Reinforcement Learning has picked up in recent years.
-
-
-@@@
-[](/data/2018/8/1/373ffed9-98f4-4f4d-8c40-54eca75fbe83.jpeg)
-@@@
-
-
-Being back in Toronto for the summer means that I had I had the chance to wander around kensington market again. This time, sans persistent summer flu. With a few fellow students in tow, Anna and I hit up [Yarns Untangled](http://yarnsuntangled.com/), the first LYS I ever visited. We picked up needles and yarn to teach some people how to knit while sharing a pitcher of beer on the patio across the street. 
-
-Against my better judgement, I picked up a few indie-dyed skeins of yarn. One from [lichen and lace](https://lichenandlace.com/)---a dyer on the east coast---and one from [fiesty fibers](https://www.instagram.com/feistyfibres)---a local Torontonian who happened to be having a trunk sale while we were in town.
-
-Who knows what the skeins will end up being. I suppose I can always teach myself how to knit socks.
-
-
-@@@
-[](/data/2018/8/1/55397df7-bf7f-47b1-aac1-e04c53d043ef.jpeg)-
-[](/data/2018/8/1/img_2326.jpg)
-@@@
-
-
-Having the chance to hit up local yarn stores with active communities reminds me of what I'm missing out on in Edmonton. YU felt like a community hub. People would would  gather on their couches, chatting with each other while they worked on whatever project they were carrying with them. 
-
-While I was waiting for a few people I sat myself down next two a couple of women and felt right at home chatting with them about how they originally started knitting and what they were currently working on. It's really refreshing to have these spaces which people can come into and join without any introduction: it's really healthy to have these communities where people can just feel at home.
-
-
-@@@
-[](/data/2018/8/1/59fcad57-4bae-4e51-837a-9c7914c5e6a0.jpeg)-
-[](/data/2018/8/1/71bdf3e8-6738-4baf-acc9-15d57be47366.jpeg)-
-[](/data/2018/8/1/16df597c-2df1-49f6-b846-9c2fa7dc745d.jpeg)
-@@@
-
-
-I have no regrets about wandering into [Little Pebbles](http://little-pebbles.com/) to have Japanese dessert _before_ meeting with some of the other students for brunch. I had this little matcha tiramisu which was carefully constructed in this little box which reminded me of sake drinking vessels. Interestingly, instead of a brandy base, at the bottom of the tiramisu was a bit of red bean paste to sweeten and balance out the earthy matcha flavours.
-
-The whole place was bright and funky without being overwhelmingly ornate. It was an unusual and pleasant surprise to see the little signs up on the tables which politely notified people that they had to put their electronics away during peak hours--an attempt to foster community and conversation.
-
-
-@@@
-[](/data/2018/8/1/8c1074f6-311e-414a-aa7b-deea9673f242.jpeg)-
-[](/data/2018/8/1/b9e2e7cf-0ef4-49e0-a2af-642d4001f865.jpeg)
-@@@
-
-
-When wandering around the city I found a whole bunch of cute ceramics, which make me regret not having kept up with pottery after highschool. Maybe I'll need to eventually fix that and take a course at Edmonton's  city arts centre.
-
-
-@@@
-[](/data/2018/8/1/69394683-9d8d-4611-9dec-0c342f1cd66b.jpeg)-
-[](/data/2018/8/1/af489a04-505b-44f9-831d-72c95efe8b00.jpeg)
-@@@
-
-
-The closest coffee shop to where I was staying was [Hopper](https://www.instagram.com/hopper_coffee_toronto). It was a cute little place with great snacks and even better espresso. In spite of being fairly spartan in terms of quantity of furniture, what they had was really funky---i.e., campbell's soup can tables. 
-
-
-@@@
-[](/data/2018/8/1/9b0b8b39-6c6e-4b3e-9f1e-30c96a4db4b1.jpeg)-
-[](/data/2018/8/1/afad5904-695c-4693-9d23-4c3d802b56a5.jpeg)
-@@@
-
-
-I finally managed to try [goldstruck](http://www.goldstruck.ca/)--a place I wanted to visit while I was interning in Toronto, but never quite had the chance to. They definitely themed the place appropriately. Walking down the stairs into the sub-terrainian coffeeshop, you're greeted by the warm glow of industrial lighting and mining-inspired decor. Even the bathroom has these massive wooden barn-doors which slide open.
-
-
-@@@
-[](/data/2018/8/1/2ab058cc-44a7-433c-a0d1-f0fbef36f2e5.jpeg)-
-[](/data/2018/8/1/5b727404-10fe-4c98-938b-7dd08eff4bd0.jpeg)-
-[](/data/2018/8/1/cfecd0dd-474c-40a6-a370-966ba79c647a.jpeg)
-@@@
-
-
-Of course, my favourite little cafe was _sorry_: a little gem that's tucked away in a corner, unapologetically making great espresso and pastries.
-"""
+    lines_2 = u'@@@[](/images/temp/IMG_1252.jpg)-[](/images/temp/IMG_1253.jpg)@@@'
 
     print run(lines, date=None)
     print run(lines_2, date=None)
