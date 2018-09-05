2fce041193e3e5084fd5ff3ab9754f9103c06cff
Alex Kearney <hi@kongaloosh.com>
2018-09-04 22:38:38 -0600

    count the likes and reposts, parse everything else

--- pysrc/webmention/webemention_checking.py
+++ pysrc/webmention/webemention_checking.py
@@ -1,24 +1,31 @@
+import requests
 """
     This module deals with retrieving webmention details
 """
 
 __author__ = 'alex'
 
-import requests
 
 def get_mentions(url):
     """We fetch all the webe"""
     mentions = []
+    reposts = 0
+    likes = 0
     try:
-        r = requests.get('http://webmention.io/api/mentions?target='+url)
+        r = requests.get('https://webmention.io/api/mentions?target='+url)
+        print 'https://webmention.io/api/mentions?target='+url
         p = r.json()
         for link in p['links']:
-            if link['data']['content']:
-		mentions.append(link['data'])
+            if link['activity']['type'] == 'like':
+                likes += 1
+            elif link['activity']['type'] == 'repost':
+                reposts += 1
+            else:
+                mentions.append(link['data'])
     except:
         pass
-    return mentions
+    return (mentions, likes, reposts)
 
 if __name__ == '__main__':
-    m = get_mentions('http://kongaloosh.com/e/2015/8/3/another-test-note-for-webmentions')
-    print(len(m))
+    (m,l,r) = get_mentions('https://kongaloosh.com/e/2018/8/12/one-of-my-favorite-parts-of-edmfolkfest-is-the-ckuaradio-tent-with-their-live-broadcasts')
+    print l,r
